67485f711d325670af11a3a382f52e631a567049
yonghoonjeon <yonghoon.jhun@gmail.com>
2023-05-17 14:50:32 +0900

    - add blank my_settings.py

---
+++ HealthGenie/my_settings.py
@@ -0,0 +1,15 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-!iy!ama-6vh+i+ml2apagt@0v(naf0d&l4n3$_x8ui9t(78_-y"
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
